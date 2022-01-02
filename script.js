2806731c2fa073a270c5ab2c24ace6f0e61b40d2
Nicole Nigro <nnigro@bowdoin.edu>
2022-01-01 16:30:25 -0800

    function debugging

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 function nycMap() {
     var n = document.getElementById("nyc");
+    var c = document.getElementById("california");
+    var m = document.getElementById("maine");
     if (n.style.display !== "none"){
         n.style.display = "none";
     } else {
         n.style.display = "block";
+        c.style.display = "none";
+        m.style.display = "none";
     }
 }
 
 function californiaMap() {
+    var n = document.getElementById("nyc");
     var c = document.getElementById("california");
+    var m = document.getElementById("maine");
     if (c.style.display !== "none"){
         c.style.display = "none";
     } else {
         c.style.display = "block";
+        n.style.display = "none";
+        m.style.display = "none";
     }
 }
 
 function maineMap(){
+    var n = document.getElementById("nyc");
+    var c = document.getElementById("california");
     var m = document.getElementById("maine");
     if (m.style.display !== "none"){
         m.style.display = "none";
     } else {
         m.style.display = "block";
+        n.style.display = "none";
+        c.style.display = "none";
     }
 }
