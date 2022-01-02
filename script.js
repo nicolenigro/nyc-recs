63e65701ec3a3c82aaeca3c79fae9a4fb8f09ab8
Nicole Nigro <nnigro@bowdoin.edu>
2022-01-01 16:08:06 -0800

    working on functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function nycMap() {
+    var n = document.getElementById("nyc");
+    if (n.style.display == "none"){
+        n.style.display = "block";
+    } else {
+        n.style.display = "none";
+    }
+}
+
+function californiaMap() {
+    return
+}
+
+function maineMap(){
+    return
+}
