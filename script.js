6e231fa3d8445553d4599e66ffee2cfe18fd0184
Nicole Nigro <nnigro@bowdoin.edu>
2022-12-19 20:24:59 -0500

    add Seattle

--- script.js
+++ script.js
@@ -2,25 +2,42 @@ function nycMap() {
     var n = document.getElementById("nyc");
     var c = document.getElementById("california");
     var m = document.getElementById("maine");
+    var s = document.getElementById("seattle");
     n.style.display = "block";
     c.style.display = "none";
     m.style.display = "none";
+    s.style.display = "none";
 }
 
 function californiaMap() {
     var n = document.getElementById("nyc");
     var c = document.getElementById("california");
     var m = document.getElementById("maine");
+    var s = document.getElementById("seattle");
     c.style.display = "block";
     n.style.display = "none";
     m.style.display = "none";
+    s.style.display = "none";
 }
 
 function maineMap(){
     var n = document.getElementById("nyc");
     var c = document.getElementById("california");
     var m = document.getElementById("maine");
+    var s = document.getElementById("seattle");
     m.style.display = "block";
+    n.style.display = "none";
+    c.style.display = "none";
+    s.style.display = "none";
+}
+
+function seattleMap(){
+    var n = document.getElementById("nyc");
+    var c = document.getElementById("california");
+    var m = document.getElementById("maine");
+    var s = document.getElementById("seattle");
+    s.style.display = "block";
+    m.style.display = "none";
     n.style.display = "none";
     c.style.display = "none";
 }
